d0e25ac89de4757e8af265ec4580d460942ef3bc
Alexiel-Alegria <alexiel.r.a2008@gmail.com>
2026-07-13 10:58:11 -0400

    Z

---
+++ python/flask/fundamentos/5.-Practica_rutas_flask/servidor_flask/app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+app = Flask(__name__)
+
+# Ruta raíz - Página de inicio
+@app.route('/')   
+def inicio():
+    return '🌎 ¡Bienvenido a nuestro servidor Flask!' 
+
+# Ruta genérica para explorar enrutamiento
+@app.route('/explorar')   
+def explorar():
+    return '🔍 ¿Qué ruta estás buscando? ¡Prueba con diferentes direcciones!' 
+
+# Rutas dinámicas para personalización
+@app.route('/perfil/<nombre>')
+def perfil(nombre):
+    print(nombre)
+    return f'👤 Bienvenid@ {nombre} a tu perfil personalizado en nuestra app.'
+
+# Ruta que repite un mensaje varias veces
+@app.route('/repite/<int:veces>/<mensaje>')
+def repetidor(veces, mensaje):
+    return (mensaje + " ") * veces
+
+# BONUS: Página de error personalizada si el usuario ingresa una ruta inexistente
+@app.errorhandler(404)
+def pagina_no_encontrada(Error):
+    return '⚠️ ¡Sobrecarga de rutas! No encontramos a dónde quieres ir, inténtalo de nuevo.', 404
+
+
+# Ejecuta el servidor
+if __name__ == "__main__":
+    app.run(debug=True)
