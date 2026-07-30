1bfeca86607e0c58a0478384652b7f9786e67c94
Alexiel-Alegria <alexiel.r.a2008@gmail.com>
2026-07-30 09:29:44 -0400

    M

--- python/flask/fundamentos/10.-Más_sobre_renderizado_de_plantillas/server.py
+++ python/flask/fundamentos/10.-Más_sobre_renderizado_de_plantillas/server.py
@@ -26,7 +26,7 @@ def renderizar_videojuegos():
     lista_videojuegos = [
         {"nombre": "Minecraft", "plataforma": "PC", "anio": 2011},
         {"nombre": "The Legend of Zelda: BotW", "plataforma": "Nintendo Switch", "anio": 2017},
-        {"nombre": "Guilty Gear Strive", "plataforma": "PS4 / PS5", "anio": 2015},
+        {"nombre": "Guilty Gear Strive", "plataforma": "PS4 / PS5", "anio": 2021},
         {"nombre": "Red Dead Redemption 2", "plataforma": "PS4 / Xbox One", "anio": 2018},
         {"nombre": "Elden Ring", "plataforma": "Multiplataforma", "anio": 2022},
         {"nombre": "Grand Theft Auto V", "plataforma": "PC / Consolas", "anio": 2013}
